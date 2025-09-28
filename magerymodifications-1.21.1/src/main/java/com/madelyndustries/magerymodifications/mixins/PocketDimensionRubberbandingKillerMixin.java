fa6113cf42b036d0e987b8683465984396e7f7a7
FireOfPower <fireofpower333@gmail.com>
2025-09-28 12:29:01 -0400

    random changes

--- magerymodifications-1.21.1/src/main/java/com/madelyndustries/magerymodifications/mixins/PocketDimensionRubberbandingKillerMixin.java
+++ magerymodifications-1.21.1/src/main/java/com/madelyndustries/magerymodifications/mixins/PocketDimensionRubberbandingKillerMixin.java
@@ -3,15 +3,12 @@ package com.madelyndustries.magerymodifications.mixins;
 import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import io.redspace.ironsspellbooks.capabilities.magic.PocketDimensionManager;
 import net.minecraft.world.level.Level;
 
-@Mixin(PocketDimensionManager.class)
+@Mixin(value = PocketDimensionManager.class, remap = false)
 public abstract class PocketDimensionRubberbandingKillerMixin {
     @WrapMethod(method="tick")
-    private void tick(Level level, Operation<Void> original){
+    public void tick(Level level, Operation<Void> original){
     }
 }
